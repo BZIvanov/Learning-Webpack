79dfb3edced9c1dfa7eb2d47a93b2c33b01b2a46
BZIvanov <ivanov.biser@gmx.com>
2023-04-09 20:36:12 +0300

    add info comment

--- Basics/05.Use-image/webpack.config.js
+++ Basics/05.Use-image/webpack.config.js
@@ -8,13 +8,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    assetModuleFilename: 'images/[name][ext]', // this way the image will go to images folder and then we specify how the file name should be constructed
+    /*
+      this way the image will go to images folder and then we specify how the file name should be constructed
+      we could also replace assetModuleFilename with publicPath which is more appropriate for working with CDN
+    */
+    assetModuleFilename: 'images/[name][ext]',
   },
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif)$/i, // we don't need rules for .js or .json files, because webpack knows how to handle them, but for images we need to specify how they should be handled
-        type: 'asset/resource',
+        type: 'asset/resource', // asset/resource will create new file/resource, but if we replace with asset/inline, instead of new file the image would be converted to base64 and added to the javascript bundle file
       },
     ],
   },
